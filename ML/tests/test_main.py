e1787dd81d0d36fca43c22f61b9a55dc6b1d0b92
Thammasorn Thammasarangkoon <poom99.2002@gmail.com>
2024-09-14 16:07:47 +0700

    Edit python CI

---
+++ ML/tests/test_main.py
@@ -0,0 +1,11 @@
+# ML/tests/test_main.py
+
+from fastapi.testclient import TestClient
+from main import app  # Use absolute import to access main.py
+
+client = TestClient(app)
+
+def test_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "server is running"}
