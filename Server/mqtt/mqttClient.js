525e3ef7f48d7e2b04b84bd3619f8712825eb46c
Thammasorn Thammasarangkoon <poom99.2002@gmail.com>
2024-09-21 21:43:06 +0700

    Add auto reconnect to Camera

--- Server/mqtt/mqttClient.js
+++ Server/mqtt/mqttClient.js
@@ -1,13 +1,16 @@
 const mqtt = require("mqtt");
 
 const connectToMQTT = (url, port, topic, messageHandler) => {
-	const client = mqtt.connect(url, { port });
-
+	console.log(`Connecting to MQTT broker at: ${url}:${port}`);
+	const uri = `mqtt://${url}:${port}`;
+	const client = mqtt.connect(uri);
 	client.on("connect", () => {
-		console.log(`Connected to MQTT broker at: ${url}`);
+		console.log(`Connected to MQTT broker at: ${uri}`);
 		client.subscribe(topic, (err) => {
 			if (!err) {
 				console.log(`Subscribed to topic: ${topic}`);
+			} else {
+				console.log(`Failed to subscribe to topic: ${topic}`);
 			}
 		});
 	});
