07c8c03f551ab06b4e49c312d0c630a6ca45fdee
Thammasorn Thammasarangkoon <poom99.2002@gmail.com>
2024-09-23 20:58:13 +0700

    Edit pytest

--- ML/tests/test_main.py
+++ ML/tests/test_main.py
@@ -1,7 +1,7 @@
+import pytest
 from fastapi.testclient import TestClient
-from app.main import app
 from unittest.mock import patch, ANY
-from app.models import ResultPayload
+from app.main import app
 from app.config import settings
 
 client = TestClient(app)
@@ -14,32 +14,35 @@ def test_root():
     assert response.json() == {"message": "server is running"}
 
 
-# Test the /ml/data/{task_id} endpoint by mocking its logic
-@patch("requests.post")
-def test_process_data(mocked_requests_post):
+@pytest.mark.asyncio
+@patch("httpx.AsyncClient.post")
+async def test_process_data(mocked_httpx_post):
     task_id = 12345
 
     # Mock the response from the Node.js server
-    mocked_requests_post.return_value.status_code = 200
-    mocked_requests_post.return_value.json.return_value = {"status": "Success"}
+    mocked_httpx_post.return_value.status_code = 200
+    mocked_httpx_post.return_value.json.return_value = {"status": "Success"}
 
     # Simulate sending the POST request to /ml/data/{task_id}
-    response = client.post(f"/ml/data/{task_id}")
+    response = client.post(f"/api/ml/data/{task_id}")
 
     # Assert that the request was successful
     assert response.status_code == 200
 
     # Check if the ML service processed and responded correctly
-    assert response.json() == {"status": "Success"}
+    assert response.json() == {
+        "status": "Success",
+        "message": f"Data for task {task_id} received. Processing in the background.",
+    }
 
     # Verify that the mock was called with the correct parameters
-    mocked_requests_post.assert_called_once()
+    mocked_httpx_post.assert_called_once()
 
     # Get the expected URL
-    expected_url = f"{settings.NODE_WEBHOOK_URL}/ml/result"
+    expected_url = f"{settings.NODE_WEBHOOK_URL}/api/ml/result"
 
     # Assert that the request to the Node.js server was made to the expected URL
-    mocked_requests_post.assert_called_with(expected_url, json=ANY)
+    mocked_httpx_post.assert_called_with(expected_url, json=ANY)
 
 
 # Test invalid data handling
